f7e2e7a2e4e8c991bf1bd35bd41b0f830dcb47c6
jason2020 <jtay20@punahou.edu>
2016-07-01 12:58:26 -1000

    Worked on project

--- js/app.js
+++ js/app.js
@@ -9,13 +9,26 @@ function runJs() {
 		clicked3: 0
 	};
 
-	buttons.addEventListener("click", clickCounter);
+	var elementLength = buttons.length;
+
+	for (var i = 0; i < elementLength; i++) {
+		buttons[i].addEventListener("click", clickCounter);
+	}
 
 	function clickCounter(evt) {
 		console.log(evt.target.id)
 		if (evt.target.id == "button1") {
 			obj.clicked1 += 1;
-
+			console.log(obj.clicked1);
+			evt.target.innerHTML = "Clicked: " + obj.clicked1;
+		} else if (evt.target.id == "button2") {
+			obj.clicked2 += 1;
+			console.log(obj.clicked2);
+			evt.target.innerHTML = "Clicked: " + obj.clicked2;
+		} else if (evt.target.id == "button3") {
+			obj.clicked3 += 1;
+			console.log(obj.clicked3);
+			evt.target.innerHTML = "Clicked: " + obj.clicked3;
 		}
 	}
 }
